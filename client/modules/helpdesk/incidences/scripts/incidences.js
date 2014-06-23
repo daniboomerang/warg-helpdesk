3318fb85f37b4e8cd13f410397a60cfc445ab3fb
daniboomerang <estevez.dani@gmail.com>
2014-06-23 20:32:50 +0200

    Updating deleted files

--- client/modules/helpdesk/incidences/scripts/incidences.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-			
-// Define main module Auth and injects all other modules as dependencies
-var incidences = angular.module('incidences',
-  [
-    'incidencesControllers',
-    'incidencesServices'
-  ]
-);
