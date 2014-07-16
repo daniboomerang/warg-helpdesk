d99c5ad6eba128aee625ba1b652c573f90438963
daniboomerang <estevez.dani@gmail.com>
2014-07-16 17:44:49 +0200

    Moving mail-listener configuration to config files

--- server/config/wh-mail-listener.js
+++ server/config/wh-mail-listener.js
@@ -1,22 +1,7 @@
-
-
+var config = require('./config');
 var MailListener = require("mail-listener2");
 
-var mailListener = new MailListener({
-  username: "estevez.dani@gmail.com",
-  password: "win2000d",
-  host: "imap.gmail.com",
-  port: 993, // imap port
-  tls: true,
-  tlsOptions: { rejectUnauthorized: false },
-  mailbox: "INBOX", // mailbox to monitor
-  searchFilter: ["UNSEEN"], // the search filter being used after an IDLE notification has been retrieved
-  markSeen: true, // all fetched email willbe marked as seen and not fetched next time
-  fetchUnreadOnStart: false, // use it only if you want to get all unread email on lib start. Default is `false`,
-  mailParserOptions: {streamAttachments: false},   // options to be passed to mailParser lib.
-  attachments: false, // download attachments as they are encountered to the project directory
-  attachmentOptions: { directory: "attachments/" } // specify a download directory for attachments
-});
+var mailListener = new MailListener(config.mailing);
 
 exports.start = function(){
   mailListener.start(); // start listening
