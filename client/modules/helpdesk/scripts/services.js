de8f65f91e99e24168df0551b26a0c0783b7e0ec
daniboomerang <estevez.dani@gmail.com>
2014-08-25 20:42:43 +0200

    Styling Menu, each module has a color. In the menu, the arrow must be style dynamic depending on the activated module

--- client/modules/helpdesk/scripts/services.js
+++ client/modules/helpdesk/scripts/services.js
@@ -8,37 +8,43 @@ helpdeskServices.service('deskMenuStyleService', function(){
 	
 	return {
 		getStyle : function(module){	
+			var style = {};
 			var moduleStyle = [];	
+			var actionsStyle = [];
 		   	if (module == 'Incidences'){
+		   		actionsStyle.create = '/modules/helpdesk/images/icons/create-16.png';
+		   		actionsStyle.openId = '/modules/helpdesk/images/icons/zoom-16.png';
+		   		actionsStyle.openList = '/modules/helpdesk/images/icons/list-16.png';
+		   		actionsStyle.statistics = '/modules/helpdesk/images/icons/statistics-16.png';
 		   		moduleStyle.image = {'background-image': 'url(/modules/helpdesk/images/icons/incidence-24.png)'};
-		   		moduleStyle.create = '/modules/helpdesk/images/icons/create-16.png';
-		   		moduleStyle.openId = '/modules/helpdesk/images/icons/zoom-16.png';
-		   		moduleStyle.openList = '/modules/helpdesk/images/icons/list-16.png';
-		   		moduleStyle.statistics = '/modules/helpdesk/images/icons/statistics-16.png';
+		   		moduleStyle.active = 'enlarged-active-module arrow-box-yellow';
 		   		moduleStyle.color = {'color': '#F4BE6E'};
-
 		   	}	
 		   	if (module == 'Inventory'){
-		   		moduleStyle.image = {'background-image': 'url(/modules/helpdesk/images/icons/book-24.png)'};
-				moduleStyle.open = '/modules/helpdesk/images/icons/inventory-16.png';
-				moduleStyle.expenses = '/modules/helpdesk/images/icons/coins-16.png';
-				moduleStyle.color = {'color': '#B54646'};	
+				actionsStyle.open = '/modules/helpdesk/images/icons/inventory-16.png';
+				actionsStyle.expenses = '/modules/helpdesk/images/icons/coins-16.png';
+				moduleStyle.image = {'background-image': 'url(/modules/helpdesk/images/icons/book-24.png)'};
+		   		moduleStyle.active = 'enlarged-active-module arrow-box-claret';
+		   		moduleStyle.color = {'color': '#B54646'};
 		   	}
 		   	if (module == 'Schools'){
-		   		moduleStyle.image = {'background-image': 'url(/modules/helpdesk/images/icons/school-24.png)'};
-		   		moduleStyle.create = '/modules/helpdesk/images/icons/create-16.png';
-		   		moduleStyle.openCode = '/modules/helpdesk/images/icons/zoom-16.png';
-		   		moduleStyle.openList = '/modules/helpdesk/images/icons/list-16.png';
+		   		actionsStyle.create = '/modules/helpdesk/images/icons/create-16.png';
+		   		actionsStyle.openCode = '/modules/helpdesk/images/icons/zoom-16.png';
+		   		actionsStyle.openList = '/modules/helpdesk/images/icons/list-16.png';
+				moduleStyle.image = {'background-image': 'url(/modules/helpdesk/images/icons/school-24.png)'};
+		   		moduleStyle.active ='enlarged-active-module arrow-box-purple';
 		   		moduleStyle.color = {'color': '#6a5acd'};
 		   	}
 		   	if (module == 'Accounts'){
-		   		moduleStyle.image = {'background-image': 'url(/modules/helpdesk/images/icons/accounts-24.png)'};
-		   		moduleStyle.create = '/modules/helpdesk/images/icons/create-16.png';
-		   		moduleStyle.import = '/modules/helpdesk/images/icons/excel-16.png';
-		   		moduleStyle.openList = '/modules/helpdesk/images/icons/list-16.png';
+		   		actionsStyle.create = '/modules/helpdesk/images/icons/create-16.png';
+		   		actionsStyle.import = '/modules/helpdesk/images/icons/excel-16.png';
+		   		actionsStyle.openList = '/modules/helpdesk/images/icons/list-16.png';
+				moduleStyle.image = {'background-image': 'url(/modules/helpdesk/images/icons/accounts-24.png)'};
+		   		moduleStyle.active ='enlarged-active-module arrow-box-green';
 		   		moduleStyle.color = {'color': '#2e8b57'};
 		   	}
-		   	return moduleStyle;
+		   	style = {moduleStyle: moduleStyle, actionsStyle: actionsStyle};
+		   	return style;
 		}
 	}
 });
@@ -47,68 +53,72 @@ helpdeskServices.service('helpdeskConfigService', function ($q, $http, menu, des
 
 	function getIncidencesMenu(actionsOnIncidences) {
      	var actions = [];
-     	var moduleMenuStyle = deskMenuStyleService.getStyle('Incidences');
+     	var style = deskMenuStyleService.getStyle('Incidences');
+     	var actionsStyle = style.actionsStyle;
      	for (var i=0; i<actionsOnIncidences.length; i++){
      		if (actionsOnIncidences.indexOf(actionsOnIncidences[i]) > -1){
      			if (actionsOnIncidences[i] == 'create')
-					actions.push({title: 'Create', state: 'helpdesk.incidences.create', style: moduleMenuStyle.create});
+					actions.push({title: 'Create', state: 'helpdesk.incidences.create', style: actionsStyle.create});
 				else if (actionsOnIncidences[i] == 'list')
-					actions.push({title: 'List', state: 'helpdesk.incidences.open.list', style: moduleMenuStyle.openList});
+					actions.push({title: 'List', state: 'helpdesk.incidences.open.list', style: actionsStyle.openList});
 				else if (actionsOnIncidences[i] == 'find')
-					actions.push({title: 'Id Search', state: 'helpdesk.incidences.open', style: moduleMenuStyle.openId});	
+					actions.push({title: 'Id Search', state: 'helpdesk.incidences.open', style: actionsStyle.openId});	
 				else if (actionsOnIncidences[i] == 'reporting')
-					actions.push({title: 'Statistics', state: 'helpdesk.incidences.statistics', style: moduleMenuStyle.statistics});
+					actions.push({title: 'Statistics', state: 'helpdesk.incidences.statistics', style: actionsStyle.statistics});
 			}		
 		}
-        var menu = {name: 'Incidences', actions: actions, image: moduleMenuStyle.image, color: moduleMenuStyle.color};
+        var menu = {name: 'Incidences', actions: actions, style: style.moduleStyle};
         return menu;
      }
 
 	function getInventoryMenu(actionsOnInventory) {
      	var actions = [];
-     	var moduleMenuStyle = deskMenuStyleService.getStyle('Inventory');
+     	var style = deskMenuStyleService.getStyle('Inventory');
+     	var actionsStyle = style.actionsStyle;
      	for (var i=0; i<actionsOnInventory.length; i++){
      		if (actionsOnInventory.indexOf(actionsOnInventory[i]) > -1){
 				if (actionsOnInventory[i] == 'list')
-					actions.push({title: 'Open', state: 'helpdesk.inventory.open', style: moduleMenuStyle.open});
+					actions.push({title: 'Open', state: 'helpdesk.inventory.open', style: actionsStyle.open});
 				else if (actionsOnInventory[i] == 'reporting')
-					actions.push({title: 'Expenses', state: 'helpdesk.inventory.expenses', style: moduleMenuStyle.expenses});
+					actions.push({title: 'Expenses', state: 'helpdesk.inventory.expenses', style: actionsStyle.expenses});
 			}		
 		}
-        var menu = {name: 'Inventory', actions: actions, style: moduleMenuStyle.module, image: moduleMenuStyle.image, color: moduleMenuStyle.color};
+        var menu = {name: 'Inventory', actions: actions, style: style.moduleStyle};
         return menu;
      }
 
      function getSchoolsMenu(actionsOnSchools) {
      	var actions = [];
-     	var moduleMenuStyle = deskMenuStyleService.getStyle('Schools');
+     	var style = deskMenuStyleService.getStyle('Schools');
+     	var actionsStyle = style.actionsStyle;
      	for (var i=0; i<actionsOnSchools.length; i++){
      		if (actionsOnSchools.indexOf(actionsOnSchools[i]) > -1){
 				if (actionsOnSchools[i] == 'create')
-					actions.push({title: 'Create', state: 'helpdesk.schools.create.school', style: moduleMenuStyle.create});
+					actions.push({title: 'Create', state: 'helpdesk.schools.create.school', style: actionsStyle.create});
 				else if (actionsOnSchools[i] == 'find'){
-					actions.push({title: 'List', state: 'helpdesk.schools.open.list', style: moduleMenuStyle.openList});
+					actions.push({title: 'List', state: 'helpdesk.schools.open.list', style: actionsStyle.openList});
 				}
 			}		
 		}
-        var menu = {name: 'Schools', actions: actions, style: moduleMenuStyle.module, image: moduleMenuStyle.image, color: moduleMenuStyle.color};
+        var menu = {name: 'Schools', actions: actions, style: style.moduleStyle};
         return menu;
      }
 
 	function getAccountsMenu(actionsOnAccounts) {
      	var actions = [];
-     	var moduleMenuStyle = deskMenuStyleService.getStyle('Accounts');
+     	var style = deskMenuStyleService.getStyle('Accounts');
+     	var actionsStyle = style.actionsStyle;
      	for (var i=0; i<actionsOnAccounts.length; i++){
      		if (actionsOnAccounts.indexOf(actionsOnAccounts[i]) > -1){
 				if (actionsOnAccounts[i] == 'create')
-					actions.push({title: 'Create', state: 'helpdesk.accounts.create.account', style: moduleMenuStyle.create});
+					actions.push({title: 'Create', state: 'helpdesk.accounts.create.account', style: actionsStyle.create});
 				else if (actionsOnAccounts[i] == 'list')
-					actions.push({title: 'List', state: 'helpdesk.accounts.open.list', style: moduleMenuStyle.openList});
+					actions.push({title: 'List', state: 'helpdesk.accounts.open.list', style: actionsStyle.openList});
 				else if (actionsOnAccounts[i] == 'import')
-					actions.push({title: 'Import Excel', state: 'helpdesk.accounts.create.list', style: moduleMenuStyle.import});
+					actions.push({title: 'Import Excel', state: 'helpdesk.accounts.create.list', style: actionsStyle.import});
 			}		
 		}
-        var menu = {name: 'Accounts', actions: actions, style: moduleMenuStyle.module, image: moduleMenuStyle.image, color: moduleMenuStyle.color};
+        var menu = {name: 'Accounts', actions: actions, style: style.moduleStyle};
         return menu;
      }
 
