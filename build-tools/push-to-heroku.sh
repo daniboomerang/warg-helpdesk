a92df910260ee0c5142a1aaee8dab985901632f7
olareoun <victor.miguez.martinez@gmail.com>
2014-09-10 17:55:50 +0200

    playing with grunt and circle

--- build-tools/push-to-heroku.sh
+++ build-tools/push-to-heroku.sh
@@ -5,4 +5,4 @@ git add -A
 git config user.name "circle"
 git config user.email "victor.miguez.martinez@gmail.com"
 git commit -m "generated www via grunt build for heroku"
-git push git@heroku.com:warg-helpdesk.git
+git push git@heroku.com:warg-helpdesk.git --force
